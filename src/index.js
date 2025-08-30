75a05f3804346567c074a9782c05f84d3b65e465
RamyGarici <ramymanilgarici@gmail.com>
2025-08-30 14:46:28 +0100

    index.js structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Webpack is running!");
+import "./styles.css";
+import home from "./home.js";
+import menu from "./menu.js";
+import about from "./contact.js";
+
+let content = document.querySelector("#content");
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+home();
+homeBtn.addEventListener("click", home);
+menuBtn.addEventListener("click", menu);
+aboutBtn.addEventListener("click", about);
